1cbfe1cda5c69b4499eed17325c2ef75bd2f9157
Rogerio Ribeiro da Cruz <rogeriorc@gmail.com>
2017-07-24 14:54:15 -0300

    #8 Bump-up sonar-scanner to the most recent version

--- bin/sonar-scanner.bat
+++ bin/sonar-scanner.bat
@@ -4,7 +4,6 @@
 @REM   JAVA_HOME - location of a JDK home dir
 @REM
 @REM Optional ENV vars:
-@REM   SONAR_SCANNER_HOME - location of runner's installed home dir
 @REM   SONAR_SCANNER_OPTS - parameters passed to the Java VM when running Sonar
 
 @echo off
@@ -14,8 +13,18 @@ set ERROR_CODE=0
 @REM set local scope for the variables with windows NT shell
 @setlocal
 
+set "scriptdir=%~dp0"
+if #%scriptdir:~-1%# == #\# set scriptdir=%scriptdir:~0,-1%
+set "SONAR_SCANNER_HOME=%scriptdir%\.."
+
 @REM ==== START VALIDATION ====
 @REM *** JAVA EXEC VALIDATION ***
+
+set use_embedded_jre=false
+if "%use_embedded_jre%" == "true" (
+  set "JAVA_HOME=%SONAR_SCANNER_HOME%\jre"
+)
+
 if not "%JAVA_HOME%" == "" goto foundJavaHome
 
 for %%i in (java.exe) do set JAVA_EXEC=%%~$PATH:i
@@ -47,34 +56,16 @@ goto error
 :foundJavaExeFromJavaHome
 set JAVA_EXEC="%JAVA_HOME%\bin\java.exe"
 
-@REM *** SONAR SCANNER HOME VALIDATION ***
 :OkJava
-if NOT "%SONAR_SCANNER_HOME%"=="" goto cleanSQScannerHome
-set SONAR_SCANNER_HOME=%~dp0..
 goto run
-
-:cleanSQScannerHome
-@REM If the property has a trailing backslash, remove it
-if "%SONAR_SCANNER_HOME:~-1%"=="\" set SONAR_SCANNER_HOME=%SONAR_SCANNER_HOME:~0,-1%
-
-@REM Check if the provided SONAR_SCANNER_HOME is a valid install dir
-IF EXIST "%SONAR_SCANNER_HOME%\lib\sonar-scanner-cli-2.8.jar" goto run
-
-echo.
-echo ERROR: SONAR_SCANNER_HOME exists but does not point to a valid install
-echo        directory: %SONAR_SCANNER_HOME%
-echo.
-goto error
-
 
 
 @REM ==== START RUN ====
 :run
-echo %SONAR_SCANNER_HOME%
 
 set PROJECT_HOME=%CD%
 
-%JAVA_EXEC% -Djava.awt.headless=true %SONAR_SCANNER_DEBUG_OPTS% %SONAR_SCANNER_OPTS% -cp "%SONAR_SCANNER_HOME%\lib\sonar-scanner-cli-2.8.jar" "-Dscanner.home=%SONAR_SCANNER_HOME%" "-Dproject.home=%PROJECT_HOME%" org.sonarsource.scanner.cli.Main %*
+%JAVA_EXEC% -Djava.awt.headless=true %SONAR_SCANNER_DEBUG_OPTS% %SONAR_SCANNER_OPTS% -cp "%SONAR_SCANNER_HOME%\lib\sonar-scanner-cli-3.0.3.778.jar" "-Dscanner.home=%SONAR_SCANNER_HOME%" "-Dproject.home=%PROJECT_HOME%" org.sonarsource.scanner.cli.Main %*
 if ERRORLEVEL 1 goto error
 goto end
 
